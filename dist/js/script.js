d32e127cdb7160ab643f44f2373331c483087b62
SergeiMatiunin <zelotmsv@gmail.com>
2023-02-25 19:59:24 +0300

    Add scrollpageup and send to email

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,6 +21,78 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
-})
+});
 
 /* persent end */
+
+// MODAL
+
+$('[data-modal=consultation]').on('click', function() {
+    $('#consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('#order').fadeIn('slow');
+    });
+  });
+
+
+
+// SEND EMAIL
+
+$('form').submit(function(e) {
+    e.preventDefault();
+
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+
+//smooth scroll and page up
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn()
+    } else {
+      $('.pageup').fadeOut();
+    }
+});
+
+$("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      const hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+});
